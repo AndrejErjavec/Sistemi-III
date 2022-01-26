936b4a347f2754fb484a824b67254367f8ab5907
Andrej Erjavec <erandrej@gmail.com.com>
2022-01-26 12:29:42 +0100

    final project

--- server-dev/index.js
+++ server-dev/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const cors=require('cors')
 const cookieParser= require('cookie-parser')
 const session= require('express-session')
+const path = require('path')
 
 
 const app=express()
@@ -13,6 +14,7 @@ const DB=require('./DB/dbConn')
 
 
 //Configs
+app.use(express.static("../client-dev/build"))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
@@ -29,6 +31,11 @@ app.use(session({
     cookie:{
         expires:60*2,sameSite:false}
 }))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, "../clinet-dev/build", "index.html"))
+    res.end()
+})
 
 //My custom routes
 const novice =require('./routes/novice')
